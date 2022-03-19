36d5f8cb074ff497edac060fd10e57f3f09dbfcf
razqyaqil <razqyaqil2@gmail.com>
2022-03-19 15:58:02 +0700

    menambahkan class Penjumlahan dengan methodJumlahTigaAngka

---
+++ modul4_1302202072/penjumlahan.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modul4_1302202072
+{
+    public class penjumlahan<T>
+    {
+
+        private T x;
+        private T y;
+        private T z;
+
+        public dynamic PenjumlahanTigaAngka<T>( T x,  T y, T z)
+        {
+            dynamic temp = 0;
+            temp = temp + x;
+            temp = temp + y;
+            temp = temp + z;
+            return temp; 
+        }
+    }
+}
